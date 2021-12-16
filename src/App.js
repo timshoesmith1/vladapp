e2fe40125d1fee48405ba26c75ea04278bab39c1
Tim S <timothy.shoesmith@gilmartins.co.uk>
2021-12-16 16:28:05 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import gilmartins_main_logo from './images/gilmartins_main_logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={gilmartins_main_logo} className='main_logo'  alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the Gilmartins Resident App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Please Log in</p>
+       <input placeholder="USER NAME" className='main_inputs'></input>
+       <input placeholder="PASSWORD" className='main_inputs'></input>
+       <button   className='login'>LOGIN</button>
       </header>
     </div>
   );
